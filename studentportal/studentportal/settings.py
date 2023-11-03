283690b8914a87067a3e9b1d3c52cbe53666f285
Md Tamjid Alam Adnan <119642555+Tamjid7401@users.noreply.github.com>
2023-11-03 12:43:29 -0500

    Update settings.py

--- studentportal/studentportal/settings.py
+++ studentportal/studentportal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ga08hu_%ic$7)y42=gjygo)a3zr=dsq)&1#0tr%73lb@z%p%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
